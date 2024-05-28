7d89f750b21f72ca820a9349518a2ca76baca90a
Ramon Nunes <ramontsdev@gmail.com>
2024-05-28 09:52:49 -0300

    feat: create resend confirmation route

--- src/main/routes/authentication.ts
+++ src/main/routes/authentication.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { confirmAccountEmailController } from "../../presentation/controllers/authentication/ConfirmAccountEmail";
+import { resendConfirmationCodeController } from "../../presentation/controllers/authentication/ResendConfirmationCode";
 import { signUpController } from "../../presentation/controllers/authentication/SignUp";
 import { adaptRoute } from "../adapters/expressRouteAdapter";
 
@@ -7,3 +8,4 @@ export const authenticationRoutes = Router();
 
 authenticationRoutes.post('/auth/sign-up', adaptRoute(signUpController));
 authenticationRoutes.post('/auth/confirm-account-email', adaptRoute(confirmAccountEmailController));
+authenticationRoutes.post('/auth/resend-confirmation-code', adaptRoute(resendConfirmationCodeController));
---
+++ src/presentation/controllers/authentication/ResendConfirmationCode.ts
@@ -0,0 +1,37 @@
+import { ResendConfirmationCodeCommand } from '@aws-sdk/client-cognito-identity-provider';
+
+import { cognitoClient } from '../../../infra/libs/cognitoClient';
+import { badRequest, ok, serverError } from '../../helpers/http-helpers';
+import { IController } from '../../protocols/controller';
+import { HttpRequest, HttpResponse } from '../../protocols/http';
+
+class ResendConfirmationCodeController implements IController {
+  constructor() {}
+
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const requiredFields = ['email'];
+      for await (const field of requiredFields)
+        if (!httpRequest.body[field]) return badRequest({ error: `${field} is required.` });
+
+      const resendConfirmationCodeCommand = new ResendConfirmationCodeCommand({
+        ClientId: process.env.COGNITO_CLIENT_ID,
+        Username: httpRequest.body.email,
+      });
+
+      const { $metadata } = await cognitoClient.send(resendConfirmationCodeCommand);
+
+      return ok({ $metadata });
+    } catch (err) {
+      const error = err as Error;
+      console.log({
+        errorName: error.name,
+        message: error.message,
+      });
+
+      return serverError();
+    }
+  }
+}
+
+export const resendConfirmationCodeController = new ResendConfirmationCodeController();
