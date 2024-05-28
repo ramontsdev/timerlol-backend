ebb614dd9ee6a4be5ab76b20ea3e65cd5bdd128c
Ramon Nunes <ramontsdev@gmail.com>
2024-05-28 10:00:34 -0300

    feat: create reset password route

--- src/main/routes/authentication.ts
+++ src/main/routes/authentication.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { confirmAccountEmailController } from "../../presentation/controllers/authentication/ConfirmAccountEmail";
 import { forgetPasswordController } from "../../presentation/controllers/authentication/ForgetPassword";
 import { resendConfirmationCodeController } from "../../presentation/controllers/authentication/ResendConfirmationCode";
+import { resetPasswordController } from "../../presentation/controllers/authentication/ResetPasswordController";
 import { signUpController } from "../../presentation/controllers/authentication/SignUp";
 import { adaptRoute } from "../adapters/expressRouteAdapter";
 
@@ -11,3 +12,4 @@ authenticationRoutes.post('/auth/sign-up', adaptRoute(signUpController));
 authenticationRoutes.post('/auth/confirm-account-email', adaptRoute(confirmAccountEmailController));
 authenticationRoutes.post('/auth/resend-confirmation-code', adaptRoute(resendConfirmationCodeController));
 authenticationRoutes.post('/auth/forget-password', adaptRoute(forgetPasswordController));
+authenticationRoutes.post('/auth/reset-password', adaptRoute(resetPasswordController));
---
+++ src/presentation/controllers/authentication/ResetPasswordController.ts
@@ -0,0 +1,48 @@
+import {
+  CodeMismatchException,
+  ConfirmForgotPasswordCommand,
+} from '@aws-sdk/client-cognito-identity-provider';
+
+import { cognitoClient } from '../../../infra/libs/cognitoClient';
+import { badRequest, ok, serverError } from '../../helpers/http-helpers';
+import { IController } from '../../protocols/controller';
+import { HttpRequest, HttpResponse } from '../../protocols/http';
+
+class ResetPasswordController implements IController {
+  constructor() {}
+
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const requiredFields = ['email', 'code', 'newPassword'];
+      for await (const field of requiredFields)
+        if (!httpRequest.body[field]) return badRequest({ error: `${field} is required.` });
+
+      const command = new ConfirmForgotPasswordCommand({
+        ClientId: process.env.COGNITO_CLIENT_ID,
+        Username: httpRequest.body.email,
+        ConfirmationCode: httpRequest.body.code,
+        Password: httpRequest.body.newPassword,
+      });
+
+      const { $metadata } = await cognitoClient.send(command);
+
+      return ok({
+        $metadata,
+      });
+    } catch (err) {
+      const error = err as Error;
+      console.log({
+        errorName: error.name,
+        message: error.message,
+      });
+
+      if (error instanceof CodeMismatchException) {
+        return badRequest({ error: 'Invalid code.' });
+      }
+
+      return serverError();
+    }
+  }
+}
+
+export const resetPasswordController = new ResetPasswordController();
